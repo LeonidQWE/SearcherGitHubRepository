ea6bf5f68a84312df9e2de71ef3e3eab70ddbfee
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2024-11-29 20:46:03 +0300

    feat: add UserTitle

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,12 +1,17 @@
 import { UserStat } from 'components/UserStat'
 import { LocalGithubUser } from 'types'
 import styles from './UserCard.module.scss'
+import { UserTitle } from 'components/UserTitle'
 
 interface UserCardProps extends LocalGithubUser {}
 
 export const UserCard = (props: UserCardProps) => {
   return (
     <div className={styles.card}>
+      <UserTitle
+        name={props.name}
+        login={props.login}
+        created={props.created}/>
       <UserStat
         repos={props.repos}
         following={props.following}
