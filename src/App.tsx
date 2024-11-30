ffe824a174b5d8b4194af7d738286db6ee6090b2
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2024-11-30 11:32:45 +0300

    feat: add github request

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
+import { useState } from 'react';
 import { Container } from 'components/Container';
 import { Header } from 'components/Header';
 import { Search } from 'components/Search';
 import { UserCard } from 'components/UserCard';
+import { GithubError, GithubUser, LocalGithubUser } from 'types';
+import { isGithubUser } from 'utils';
+import { extractGithubUder } from 'utils';
+
+const GITHUB_USER_URL = 'https://api.github.com/users/';
 
 export default function App () {
+  const [user, setUser] = useState<LocalGithubUser | null>(null);
+
+  const fetchUser = async (text: string) => {
+    const url = `${GITHUB_USER_URL}${text}`;
+    const response = await fetch(url);
+    const data = await response.json() as GithubUser | GithubError;
+
+    if (isGithubUser(data)) {
+      setUser(extractGithubUder(data));
+    } else {
+      setUser(null);
+    }
+  }
+
   return (
     <Container>
       <Header />
-      <Search hasError={true} onSubmit={() => {}}/>
-      <UserCard/>
+      <Search hasError={!user} onSubmit={fetchUser}/>
+      {user ? <UserCard {...user}/> : <span>Not Found</span>}
     </Container>
   )
 }
---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export * from './extract-github-user';
+export * from './typeguards';
